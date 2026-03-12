6805d32915c0003add81bad138fd63fe0e01628b
Vincent Reynaert <reynaert.vincent@laposte.net>
2026-03-12 17:06:19 +0100

    update questions

--- qcm-L2JV/questions.js
+++ qcm-L2JV/questions.js
@@ -49,7 +49,7 @@ export const QUIZ = {
       image: "assets/unity_editor_red.png",
       image_alt: "Illustration où unity a son interface teintée de rouge mis à part le bouton play actuellement bleu.",
       multi: true,
-      points: 2,
+      points: 1,
       correct: [0, 2],
       options: [
         "Si je modifie des GameObjects dans ma scène les modifications ne seront pas enregistrables.",
@@ -57,6 +57,94 @@ export const QUIZ = {
         "Si je modifie des Prefabs il faut penser à appliquer les modifications effectuées avant de quitter le playmode.",
         "Si je modifie des GameObjects dans ma scène je ne verrais pas les modifications en direct.",
         "Il y a une erreur dans le code."
+      ]
+    },
+
+    {
+      id: "q04",
+      text: "J’ai décidé de créer un Visual Novel et tout se passe bien dans le développement jusqu’au moment où, lorsque j’appuie sur le bouton “Next” servant à afficher la suite du dialogue, l’erreur suivante de produit. Pour faire disparaître cette erreur de la console, je pourrais :",
+      image: "assets/IndexOOR.png",
+      image_alt: "Illustration d'une erreur en console disant : IndexOutOfRange",
+      multi: true,
+      points: 1,
+      correct: [0, 1, 2, 3],
+      options: [
+        "Rajouter des dialogues de façon procédurale. Cela permettrait d'appuyer à l’infinie sur le bouton “Next” sans avoir cette erreur.",
+        "Prendre le script qui pose problème (MainGame.cs) et le mettre dans la corbeille. (Pas de script pas d'erreur)...",
+        "Modifier le code et ajouter une condition pour vérifier que si je n'ai plus de dialogue ensuite le bouton se désactive.",
+        "Faire un script qui fait se déplacer le bouton pour que le joueur ne puisse pas cliquer dessus. (Pas de clic pas d'erreur)...",
+        "Relancer Unity et croiser les doigts pour que cela fonctionne."
+      ]
+    },
+
+    {
+      id: "q05",
+      text: "Je souhaite, comme sur l’image après, ajouter une barre de vie à un monstre. Celle-ci devra être en UI, visuellement positionnée sous le monstre qui est affiché via un Sprite Renderer. Que dois-je faire ?",
+      image: "assets/CanvasOrder2.png",
+      image_alt: "Illustration d'un monstre avec une barre de point de vie partiellement remplie et située en-dessous.",
+      multi: true,
+      points: 1,
+      correct: [0],
+      options: [
+        "Le plus simple est d’ajouter un Canvas qui sera affiché en WorldSpace afin de bien le positionner sous le monstre, ce Canvas comportera deux Images en enfant, une de background et une de remplissage.",
+        "J’ajoute deux SpriteRenderers en enfant d’un EventSystem en WorldSpace, un pour le background et un en mode filled pour remplir ou vider la jauge de vie.",
+        "Il n’est pas possible de créer une barre de vie en UI.",
+        "Je dois absolument coder un component custom qui va afficher une texture adaptative.",
+        "Le plus simple est d'ajouter un Canvas qui sera affiché en Overlay pour que les éléments de jeux ne puissent pas obstruer la vue du joueur sur cette barre de vie. Ce Canvas comportera une seule Image en enfant qui sera en mode fill/remplissage."
+      ]
+    },
+
+    {
+      id: "q06",
+      text: "J’ai réussi à créer ma jauge de PV mais le texte qui devrait normalement s’afficher dessus n’apparaît pas. Pourquoi ?",
+      image: "assets/CanvasOrder.png",
+      image_alt: "Illustration double, à gauche une capture de l'éditeur Unity avec le Monster qui en enfant possède un Canvas contenant LifePoint et LifeBar dans cette ordre, LifeBar contenant LifeFill, à droite un monstre avec une barre de point de vie partiellement remplie et située en-dessous mais sans texte.",
+      multi: true,
+      points: 1,
+      correct: [0,1],
+      options: [
+        "Dans ma hiérarchie, je dois descendre LifePoint sous LifeBar et LifeFill.",
+        "Je n'ai pas importé les assets par défaut de TMPro ou j'ai oublié de mettre une police à mon Text.",
+        "Il faut que je modifie l'option “order in layer” de mon Text “LifePoint”.",
+        "Il faut que je modifie l’option “order in layer” de mon Image “LifeBar”.",
+        "Il faut que je modifie l’option “order in layer” de mon Image “LifeFill”.",
+        "Je dois mettre LifeBar enfant de LifePoint."
+      ]
+    },
+
+    {
+      id: "q07",
+      text: "Soit le code suivant :\nLorsque je lance le jeu rien ne s’affiche dans la console, quelle(s) peu(ven)t être la(les) raison(s) ?",
+      image: "assets/no_print.png",
+      image_alt: "Capture d'un bout de code.",
+      multi: true,
+      points: 1,
+      correct: [0,1],
+      options: [
+        "Aucune des autres réponses n’est une raison valable.",
+        "La classe devrait être renommée Player.",
+        "Il faut retirer le void.",
+        "Il faut absolument mettre un “print” à la place de “Debug.Log”.",
+        "Il faut mettre le Debug.Log dans le Start et non dans l’Update.",
+        "Le GameObject dans la scène surlequel est placé ce component s'appelle “_Script” alors qu'il devrait avoir le même nom que le Component."
+      ]
+    },
+
+    {
+      id: "q08",
+      text: "Soit le code suivant :\nQuelle(s) affirmation(s) est(sont) vraie(s) ?",
+      image: "assets/raycast2d.png",
+      image_alt: "Capture d'un bout de code.",
+      multi: true,
+      points: 1,
+      correct: [0,1],
+      options: [
+        "Aucune des autres réponses n’est une raison valable.",
+        "La classe devrait être renommée Player.",
+        "Il faut retirer le void.",
+        "Il faut absolument mettre un “print” à la place de “Debug.Log”.",
+        "Il faut mettre le Debug.Log dans le Start et non dans l’Update.",
+        "Le GameObject dans la scène surlequel est placé ce component s'appelle “_Script” alors qu'il devrait avoir le même nom que le Component."
       ]
     }
   ]
